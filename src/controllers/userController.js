d0e2b742d12d63de58bb97c20479932dae3a3fd2
obeaune <linecora86@gmail.com>
2022-06-23 02:36:08 -0300

    the application now has the GET /user/:id endpoint (with authentication).

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,15 +16,26 @@ const create = async (req, res) => {
   // [it is not possible to register with an existing email]
   const alreadyExists = await User.findOne({ where: { email } });
   if (alreadyExists) return res.status(409).json({ message: 'User already registered' });
-
+  
   // [is it possible to register a user successfully]
   await User.create({ displayName, email, password, image });
-
   const token = generateToken(email);
   return res.status(201).json({ token });
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findOne({
+    where: { id },
+    attributes: ['id', 'displayName', 'email', 'image'],
+  });
+  if (!user) return res.status(404).json({ message: 'User does not exist' });
+  return res.status(200).json(user);
 };
 
 module.exports = {
   getAll,
   create,
+  getById,
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,8 +6,10 @@ const user = require('../controllers/userController');
 const validateUser = require('../middlewares/validateUser');
 const validateJWT = require('../middlewares/validateJWT');
 
+route.post('/', validateUser, user.create);
+
 route.get('/', validateJWT, user.getAll);
 
-route.post('/', validateUser, user.create);
+route.get('/:id', validateJWT, user.getById);
 
 module.exports = route;
