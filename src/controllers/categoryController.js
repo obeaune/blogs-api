24809848b7503064565db7d1fcf1cf609197aebf
obeaune <linecora86@gmail.com>
2022-06-27 22:19:21 -0300

    feat(categories): putting comments in code

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -7,8 +7,13 @@ const getAll = async (_req, res) => {
 
 const create = async (req, res) => {
   const { name } = req.body;
+
   const response = await Service.create(name);
+
+  // [it is not possible to register a category without the name field]
   if (!response) return res.status(400).json({ message: '"name" is required' });
+
+  // [it is possible to register a category successfully]
   return res.status(201).json(response);
 };
 
